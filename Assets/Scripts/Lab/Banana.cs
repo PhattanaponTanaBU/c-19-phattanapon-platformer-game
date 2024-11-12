d47faaac79ab455a0315138f83df54750e046d83
Owen6936TH <owen.30.10.2005@gmail.com>
2024-11-13 00:04:11 +0700

    Minor Adjustment

--- Assets/Scripts/Lab/Banana.cs
+++ Assets/Scripts/Lab/Banana.cs
@@ -6,11 +6,10 @@ public class Banana : Weapon
 {
     //Attributes
     [SerializeField] private float speed;
-    private float direction;
 
     private void Start()
     {
-        direction = GetShootDirection();
+        speed *= GetShootDirection();
     }
 
     //Method
@@ -21,7 +20,7 @@ public class Banana : Weapon
 
     public override void Move()
     {
-        float newX = transform.position.x + (speed * Time.fixedDeltaTime * direction);
+        float newX = transform.position.x + (speed * Time.fixedDeltaTime);
         float newY = transform.position.y;
         this.transform.position = new Vector2(newX, newY);
 
